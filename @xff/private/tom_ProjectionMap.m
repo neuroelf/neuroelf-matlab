aade3da3aaa81aa66d82ed130a6a9cd2256e6ccb
Weber <weberj3@mskcc.org>
2022-07-20 10:21:20 -0400

    Updated coloring

--- @xff/private/tom_ProjectionMap.m
+++ @xff/private/tom_ProjectionMap.m
@@ -6,18 +6,22 @@ function smp = tom_ProjectionMap(xo, opts)
 % Input fields:
 %
 %       opts        optional settings
+%        .colors    Cx3 array of RGB colors to set maps, default: hues
+%        .huefactor 1x1 double, default 2 (separating front and back)
 %
 % Output fields:
 %
 %       smp         SMP object with projection maps
+%
+% Using: hsvconv.
 
 % Version:  v1.1
-% Build:    21120216
-% Date:     Dec-02 2021, 4:32 PM EST
+% Build:    22072010
+% Date:     Jul-20 2022, 10:02 AM EST
 % Author:   Jochen Weber, NeuroElf.net, NYC, NY, USA
 % URL/Info: http://neuroelf.net/
 %
-% Copyright (c) 2021, Jochen Weber
+% Copyright (c) 2021, 2022, Jochen Weber
 % All rights reserved.
 %
 % Redistribution and use in source and binary forms, with or without
@@ -42,12 +46,26 @@ function smp = tom_ProjectionMap(xo, opts)
 % (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+% library function
+global ne_methods;
+hsvconv = ne_methods.hsvconv;
+
 % check arguments
 if nargin < 1 || numel(xo) ~= 1 || ~xffisobject(xo, true, 'tom')
     error('neuroelf:xff:badArgument', 'Invalid call to %s.', mfilename);
 end
 if nargin < 2 || ~isstruct(opts) || numel(opts) ~= 1
     opts = struct;
+end
+if ~isfield(opts, 'colors') || ~isa(opts.colors, 'double') || ...
+    ndims(opts.colors) > 2 || size(opts.colors, 2) ~= 3 || ...
+    any(isinf(opts.colors(:)) | isnan(opts.colors(:)) | opts.colors(:) < 0)
+    opts.colors = [];
+end
+if ~isfield(opts, 'huefactor') || ~isa(opts.huefactor, 'double') || ...
+    numel(opts.huefactor) ~= 1 || isinf(opts.huefactor) || ...
+    isnan(opts.huefactor) || opts.huefactor <= 0
+    opts.huefactor = 2;
 end
 
 % access TOM properties
@@ -59,6 +77,14 @@ tv = tom.TriangleVertex;
 % generate map content
 nummaps = max(tam);
 m = zeros(tom.NrOfVertices, nummaps);
+
+% generate colors
+if isempty(opts.colors) || size(opts.colors, 1) < nummaps
+    opts.colors = zeros(nummaps, 3);
+    for c = 1:nummaps
+        opts.colors(c, :) = hsvconv([mod(opts.huefactor * (c-1) / nummaps, 1), 1, 1], 1);
+    end
+end
 
 % iterate over vertices
 for c = size(ctm,1):-1:1
@@ -84,5 +110,7 @@ for c = 1:nummaps
     smpc.Map(c).LowerThreshold = 1;
     smpc.Map(c).UpperThreshold = 4;
     smpc.Map(c).Name = sprintf('Texture %d', c);
+    smpc.Map(c).RGBLowerThreshPos = opts.colors(c, :);
+    smpc.Map(c).RGBUpperThreshPos = opts.colors(c, :);
 end
 smp.C = smpc;
