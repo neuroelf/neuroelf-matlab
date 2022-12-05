581e918a7d1eda8b69ea094cc1e6d8c82a918efb
Weber <weberj3@mskcc.org>
2022-12-05 15:47:02 -0500

    First working version

--- @neuroelf/private/disttrans.m
+++ @neuroelf/private/disttrans.m
@@ -1,5 +1,5 @@
 function x = disttrans(x, opts)
-%DISTTRANS  Distance transform binary data
+%DISTTRANS  Distance transform binary data (2D/3D)
 %
 % D = DISTTRANS(M) creates the distance transform of binary mask M
 %
@@ -36,51 +36,79 @@ end
 xso = size(x);
 x = squeeze(x);
 xs = size(x);
+if numel(xs) > 3
+    error('neuroelf:badArgument', 'Function only supports up to 3D input.');
+end
 
 % special cases
 if all(x(:))
-    x = (1/0) .* ones(xs);
+    x = (1/0) .* ones(xso);
     return;
 elseif ~any(x(:))
-    x = zeros(xs);
+    x = zeros(xso);
     return;
 end
 
-% take negative mask (for growth limitation)
-nx = ~x;
+% cluster positive side
+connf = find('fev' == conn);
+if ndims(x) < 3
+    connf = min(5, connf + 3);
+end
+
+% iterate over clusters
 d = zeros(xs);
 nd = numel(xs);
+[cs, cv, cl, cc] = clustercoordsc(x, connf);
 ar = struct('type', '()', 'subs', {repmat({':'}, [1, nd])});
-am = ar.subs;
-an = ar.subs;
-for dc = 1:nd
-    am{dc} = 1:(xs(dc)-1);
-    an{dc} = 2:xs(dc);
-end
-
-% dilate negative mask
-dnx = nx;
-for dc = 1:nd
-    ars = ar;
-    art = ar;
-    ars.subs{dc} = am{dc};
-    art.subs{dc} = an{dc};
-    dnx = subsasgn(dnx, art, subsref(dnx, art) | subsref(nx, ars));
-    ars.subs{dc} = an{dc};
-    art.subs{dc} = am{dc};
-    dnx = subsasgn(dnx, art, subsref(dnx, art) | subsref(nx, ars));
-end
-if conn == 'e' ||
-elseif conn == 'v' && nd > 3
-end
-
-% only border of negative mask!
-dnx = dnx & ~nx;
-
-
-% process until everything is marked up
-while ~all(nx(:))
+for c = 1:numel(cs)
     
+    % cutout
+    nc = size(cc{c}, 1);
+    mnc = min(cc{c});
+    mxc = max(cc{c});
+    for dc = 1:nd
+        ar.subs{dc} = mnc(dc):mxc(dc);
+    end
+    cx = subsref(cv, ar) == c;
+    
+    % embed into zeros, create and find boundary pixels
+    cxx = false(size(cx) + 2);
+    if nd < 3
+        cxx(2:end-1, 2:end-1) = cx;
+        cxx(1:end-2, 2:end-1) = cxx(1:end-2, 2:end-1) | cx;
+        cxx(3:end, 2:end-1) = cxx(3:end, 2:end-1) | cx;
+        cxx(2:end-1, 1:end-2) = cxx(2:end-1, 1:end-2) | cx;
+        cxx(2:end-1, 3:end) = cxx(2:end-1, 3:end) | cx;
+        cxx(2:end-1, 2:end-1) = cxx(2:end-1, 2:end-1) & ~cx;
+        [cxx, cxy] = ind2sub(size(cxx), find(cxx(:)));
+        cxx = [cxx, cxy] - 1;
+    else
+        cxx(2:end-1, 2:end-1, 2:end-1) = cx;
+        cxx(1:end-2, 2:end-1, 2:end-1) = cxx(1:end-2, 2:end-1, 2:end-1) | cx;
+        cxx(3:end, 2:end-1, 2:end-1) = cxx(3:end, 2:end-1, 2:end-1) | cx;
+        cxx(2:end-1, 1:end-2, 2:end-1) = cxx(2:end-1, 1:end-2, 2:end-1) | cx;
+        cxx(2:end-1, 3:end, 2:end-1) = cxx(2:end-1, 3:end, 2:end-1) | cx;
+        cxx(2:end-1, 2:end-1, 1:end-2) = cxx(2:end-1, 2:end-1, 1:end-2) | cx;
+        cxx(2:end-1, 2:end-1, 3:end) = cxx(2:end-1, 2:end-1, 3:end) | cx;
+        cxx(2:end-1, 2:end-1, 2:end-1) = cxx(2:end-1, 2:end-1, 2:end-1) & ~cx;
+        [cxx, cxy, cxz] = find(cxx);
+        cxx = [cxx, cxy, cxz] - 1;
+    end
+    nbc = size(cxx, 1);
+    
+    % compute distances
+    ds = zeros(nc, nbc);
+    for dc = 1:nd
+        ds = ds + ((cc{c}(:, dc) - (mnc(dc) - 1)) * ones(1, nbc) - ones(nc, 1) * cxx(:, dc)') .^ 2;
+    end
+    ds = sqrt(min(ds, [], 2));
+    
+    % set in partial volume
+    dx = subsref(d, ar);
+    dx(cx) = ds;
+    
+    % set in output
+    d = subsasgn(d, ar, dx);
 end
 
 % resize again
