5e2a236859fef25204f8030d9a741cf71cd8761a
Weber <weberj3@mskcc.org>
2023-02-10 14:51:00 -0500

    Added TRK conversion

---
+++ @xff/private/srf_AsTRK.m
@@ -0,0 +1,66 @@
+function trk = srf_AsTRK(xo)
+% SRF::AsTRK  - create a TRK object from a surface
+%
+% FORMAT:       srf.AsTRK();
+%
+% No input fields.
+
+% Version:  v1.1
+% Build:    23021013
+% Date:     Feb-10 2023, 1:47 PM EST
+% Author:   Jochen Weber, NeuroElf.net
+% URL/Info: http://neuroelf.net/
+%
+% Copyright (c) 2023, Jochen Weber
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in the
+%       documentation and/or other materials provided with the distribution.
+%     * Neither the name of Columbia University nor the
+%       names of its contributors may be used to endorse or promote products
+%       derived from this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+% ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+% DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+% ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+% check input arguments
+if nargin < 1 || numel(xo) ~= 1 || ~xffisobject(xo, true, 'srf')
+    error('neuroelf:xff:badArgument', 'Invalid call to %s.', mfilename);
+end
+bc = xo.C;
+if ~isfield(bc.RunTimeVars, 'FiberStarts')
+    error('neuroelf:xff:badArgument', 'Convertion to TRK requires Fiber-SRF.');
+end
+rtv = bc.RunTimeVars;
+fs = rtv.FiberStarts;
+
+% convert
+trk = xff('new:trk');
+oldid = trk.C.RunTimeVars.xffID;
+trk.C.RunTimeVars = bc.RunTimeVars;
+trk.C.RunTimeVars.xffID = oldid;
+trk.C.RunTimeVars.SourceSRFFilename = xo.F;
+trk.C.NrOfTracts = numel(fs) - 1;
+trk.C.NrOfScalarsPerPoint = 3;
+trk.C.ScalarNames = {'RGB-R'; 'RGB-G'; 'RGB-B'};
+trk.C.VoxelToRASMatrix = rtv.TransVoxelToTal;
+t = repmat(trk.C.Tracts(1), trk.C.NrOfTracts, 1);
+for tc = 1:numel(t)
+    t(tc).NrOfPoints = fs(tc+1) - (fs(tc) + 1);
+    t(tc).Points = 256 - bc.VertexCoordinate(fs(tc):fs(tc+1)-2, [3, 1, 2]);
+    t(tc).Values = (1 / 255) .* bc.VertexColor(fs(tc):fs(tc+1)-2, :);
+end
+trk.C.Tracts = t;
