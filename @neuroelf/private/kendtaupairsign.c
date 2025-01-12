305fd41337ac6da82da5375657377aa42622dc20
neuroelf <cj.hammond.gm@gmail.com>
2025-01-11 22:19:54 -0500

    Fixed type for Apple Silicon compile.

--- @neuroelf/private/kendtaupairsign.c
+++ @neuroelf/private/kendtaupairsign.c
@@ -16,13 +16,13 @@ Output fields:
 Note: the order in which pairs are made is (1;2), (1;3), ..., (1;n),
       (2;3), ... (n-1;n)
 
-% Version:  v0.9a
-% Build:    11102110
-% Date:     May-17 2010, 10:48 AM EST
-% Author:   Jochen Weber, SCAN Unit, Columbia University, NYC, NY, USA
+% Version:  v1.1b
+% Build:    25011122
+% Date:     Jan-11 2025, 10:20 PM EST
+% Author:   Jochen Weber, NeuroElf
 % URL/Info: http://neuroelf.net/
 
-Copyright (c) 2010, Jochen Weber
+Copyright (c) 2010, 2025, Jochen Weber
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -97,8 +97,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	const int *dim;
-    int odim[64];
+	const mwSize *dim;
+    mwSize odim[64];
     const double *dbl, *idbl;
     const float *sng, *isng;
     const signed char *si8, *isi8;
